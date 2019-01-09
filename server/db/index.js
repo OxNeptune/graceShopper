06c567235e681e86a9a2ab7394a9a5ebf20329f8
Emily Zerbe <emilybzerbe@gmail.com>
2019-01-09 11:29:34 -0500

    Database Seeded

--- server/db/index.js
+++ server/db/index.js
@@ -1,15 +1,6 @@
 const db = require('./db')
-const User = require('./models/user')
-const Plant = require('./models/plant')
 
-// register models
-// require('./models')
+//register models
+require('./models')
 
-Plant.belongsTo(User)
-User.hasMany(Plant)
-
-module.exports = {
-  db,
-  User,
-  Plant
-}
+module.exports = db
