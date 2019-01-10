98e0019459d84f87242bc034710ecfa5fd6f6f02
MCClarke <mcarlotclarke@gmail.com>
2019-01-10 16:08:46 -0500

    Feature: User-slice model update, specs, routes

--- server/api/users.js
+++ server/api/users.js
@@ -8,7 +8,18 @@ router.get('/', async (req, res, next) => {
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'email']
+      attributes: ['id', 'firstName', 'lastName', 'email']
+    })
+    res.json(users)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const users = await User.findById(req.params.id, {
+      attributes: ['id', 'firstName', 'lastName', 'email']
     })
     res.json(users)
   } catch (err) {
