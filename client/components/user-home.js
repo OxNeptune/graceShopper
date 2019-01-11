e6afff58dfce87ae91b58993c26ecf4af330b580
MCClarke <mcarlotclarke@gmail.com>
2019-01-11 14:39:49 -0500

    Feature: user-home features first draft fixes

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,26 +1,73 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+// import orders from '../store/orders'
 
 /**
  * COMPONENT
  */
-export const UserHome = props => {
-  const {email} = props
+class UserHome extends Component {
+  constructor(props) {
+    super(props)
+    this.showOrders = this.showOrders.bind(this)
+  }
 
-  return (
-    <div>
-      <h3>Welcome, {email}</h3>
-    </div>
-  )
+  showOrders() {
+    console.log('Many orders, yay, I bought a monkey!')
+  }
+
+  render() {
+    const {firstName} = this.props
+
+    return (
+      <div>
+        <h2>Welcome, {firstName}</h2>
+        <div className="user-home-features">
+          <button type="button" onClick={this.showOrders}>
+            Orders
+            <div>
+              {/* {orders.length ? (
+                orders.map(order => (
+                  <Link to={`/orders/${order.id}`} key={order.id}>
+                    <Order />
+                  </Link>
+                ))
+              ) : (
+                <h4>You have no orders at this time</h4>
+              )} */}
+            </div>
+          </button>
+          <button type="button">Your Addresses</button>
+          <button type="button">Login/Security</button>
+          <button type="button">Payment Options</button>
+        </div>
+      </div>
+    )
+  }
 }
+
+// export const UserHome = props => {
+//   const {firstName} = props
+
+//   return (
+//     <div>
+//       <h2>Welcome, {firstName}</h2>
+//       <div className="user-home-features">
+//         <button type="button">Orders</button>
+//         <button type="button">Your Addresses</button>
+//         <button type="button">Login/Security</button>
+//         <button type="button">Payment Options</button>
+//       </div>
+//     </div>
+//   )
+// }
 
 /**
  * CONTAINER
  */
 const mapState = state => {
   return {
-    email: state.user.email
+    firstName: state.user.firstName
   }
 }
 
@@ -30,5 +77,5 @@ export default connect(mapState)(UserHome)
  * PROP TYPES
  */
 UserHome.propTypes = {
-  email: PropTypes.string
+  firstName: PropTypes.string
 }
