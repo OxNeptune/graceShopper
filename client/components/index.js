fbd93fd3a25f6174613d3a0a8811ca685f20a4b4
MCClarke <mcarlotclarke@gmail.com>
2019-01-14 12:32:05 -0500

    Feature: commmiting all updates3

--- client/components/index.js
+++ client/components/index.js
@@ -3,8 +3,8 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export {default as Navbar} from './Navbar'
-export {default as UserHome} from './User-home'
+export {default as Navbar} from './navbar'
+export {default as UserHome} from './user-home'
 export {Login, Signup} from './Auth-form'
 export {default as PlantList} from './PlantList'
 export {default as SinglePlant} from './SinglePlant'
